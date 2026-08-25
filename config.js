e53ea3f4c0aa057ab6ddbcc099ea3f2c82e3984c
alvin921play <alvin921play@gmail.com>
2026-08-25 19:59:39 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// 只填這兩個。
+// Publishable key 本來就是給瀏覽器使用的；不要填 Secret key / service_role。
+window.APP_CONFIG = {
+  SUPABASE_URL: "貼上你的 Project URL",
+  SUPABASE_PUBLISHABLE_KEY: "貼上你的 Publishable key"
+};
