bf165d6d6a206e8f1bb1db0fc3c13b1c9a069f1b
alvin921play <alvin921play@gmail.com>
2026-08-26 11:39:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-// 只填這兩個。
-// Publishable key 本來就是給瀏覽器使用的；不要填 Secret key / service_role。
-window.APP_CONFIG = {
-  SUPABASE_URL: "貼上你的 Project URL",
-  SUPABASE_PUBLISHABLE_KEY: "貼上你的 Publishable key"
-};
+
